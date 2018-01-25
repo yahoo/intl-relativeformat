6754cf9f89fcbdff6bd28c50b2b207e9cace1a4f
Sebastian Werner <s.werner@sebastian-software.de>
2018-01-25 19:03:51 +0100

    Lint fixes

--- src/diff.js
+++ src/diff.js
@@ -17,28 +17,28 @@ function daysToYears(days) {
 // https://github.com/date-fns/date-fns
 // MIT © Sasha Koss
 
-var MILLISECONDS_IN_MINUTE = 60000
-var MILLISECONDS_IN_DAY = 86400000
+var MILLISECONDS_IN_MINUTE = 60000;
+var MILLISECONDS_IN_DAY = 86400000;
 
 function startOfDay (dirtyDate) {
-    var date = new Date(dirtyDate)
-    date.setHours(0, 0, 0, 0)
-    return date
+    var date = new Date(dirtyDate);
+    date.setHours(0, 0, 0, 0);
+    return date;
 }
 
 function differenceInCalendarDays (dirtyDateLeft, dirtyDateRight) {
-    var startOfDayLeft = startOfDay(dirtyDateLeft)
-    var startOfDayRight = startOfDay(dirtyDateRight)
+    var startOfDayLeft = startOfDay(dirtyDateLeft);
+    var startOfDayRight = startOfDay(dirtyDateRight);
 
     var timestampLeft = startOfDayLeft.getTime() -
-        startOfDayLeft.getTimezoneOffset() * MILLISECONDS_IN_MINUTE
+        startOfDayLeft.getTimezoneOffset() * MILLISECONDS_IN_MINUTE;
     var timestampRight = startOfDayRight.getTime() -
-        startOfDayRight.getTimezoneOffset() * MILLISECONDS_IN_MINUTE
+        startOfDayRight.getTimezoneOffset() * MILLISECONDS_IN_MINUTE;
 
     // Round the number of days to the nearest integer
     // because the number of milliseconds in a day is not constant
     // (e.g. it's different in the day of the daylight saving time clock shift)
-    return Math.round((timestampLeft - timestampRight) / MILLISECONDS_IN_DAY)
+    return Math.round((timestampLeft - timestampRight) / MILLISECONDS_IN_DAY);
 }
 
 export default function (from, to) {
